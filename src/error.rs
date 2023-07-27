39384fc94b2f3ca5a41d31f022a6203df8f333aa
HimbeerserverDE <himbeerserverde@gmail.com>
2023-07-27 17:36:51 +0200

    test session creation

--- src/error.rs
+++ src/error.rs
@@ -7,6 +7,9 @@ use thiserror::Error;
 pub enum Error {
     #[error("io: {0}")]
     Io(#[from] io::Error),
+
+    #[error("rsdsl_pppoe2_sys: {0}")]
+    RsdslPppoe2Sys(#[from] rsdsl_pppoe2_sys::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use rsdsl_pppoe2::Result;
+
+fn main() -> Result<()> {
+    let (_a, _b, _c) =
+        rsdsl_pppoe2_sys::new_session("wlan0", [0x41, 0x41, 0x41, 0x41, 0x41, 0x41].into(), 1)?;
+    loop {}
 }
