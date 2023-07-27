5afd418c05ee8c84d7236ec0fae7e90554cfbdcd
HimbeerserverDE <himbeerserverde@gmail.com>
2023-07-27 16:14:12 +0200

    add wrapper for C pppoe2_new_discovery_socket

--- pppoe2_sys/src/lib.rs
+++ pppoe2_sys/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use error::*;
+
+use std::ffi::CString;
+use std::os::fd::FromRawFd;
+
+use ppproperly::MacAddr;
+use socket2::Socket;
+
+mod internal {
+    include!(concat!(env!("OUT_DIR"), "/pppoe2_bindings.rs"));
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod error {
+    use std::{array, ffi, io};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use thiserror::Error;
+
+    #[derive(Debug, Error)]
+    pub enum Error {
+        #[error("nul termination: {0}")]
+        Nul(#[from] ffi::NulError),
+        #[error("io: {0}")]
+        Io(#[from] io::Error),
+        #[error("array type conversion: {0}")]
+        TryFromSlice(#[from] array::TryFromSliceError),
     }
+
+    pub type Result<T> = std::result::Result<T, Error>;
+}
+
+pub fn new_discovery_socket(interface: &str) -> Result<(Socket, MacAddr)> {
+    let ifname = CString::new(interface)?.into_raw();
+    let hwaddr = CString::new([0; libc::IFNAMSIZ])?.into_raw();
+
+    let fd = unsafe { internal::pppoe2_create_discovery_socket(ifname, hwaddr) };
+
+    let _ = unsafe { CString::from_raw(ifname) };
+    let hwaddr = unsafe { CString::from_raw(hwaddr) };
+
+    let sock = unsafe { Socket::from_raw_fd(fd) };
+
+    Ok((sock, <[u8; 6]>::try_from(hwaddr.as_bytes())?.into()))
 }
